290c487ecac9c3d64c8d23dce9451667804688fd
Caio <caiojcarvalho@gmail.com>
2018-03-20 18:07:54 -0300

    Removing functional and member method pointer

--- iteratorsuperpower.h
+++ iteratorsuperpower.h
@@ -1,20 +1,13 @@
 #ifndef ITERATORSUPERPOWER_H
 #define ITERATORSUPERPOWER_H
 
-#include <functional>
-
 using namespace std;
 
 template<template<class> class Collection, class Class, typename Type>
 class IteratorSuperPower
 {
-    typedef Type (Class::*MemberMethod)() const;
-
 public:
-    IteratorSuperPower(const Collection<Class *> &collection,
-                       const char *attribute,
-                       const QString &op,
-                       Type value);
+    IteratorSuperPower(const Collection<Class *> &collection, const char *attribute, const QString &op, Type value);
     void first();
     void next();
     bool hasNext() const;
